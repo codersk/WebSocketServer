d115b321a7ac34284e6c201d4cbacb2e46993bc0
Sandeep <sandeep_kumbhar@mazakindia.com>
2018-06-03 12:41:12 +0530

    Duplicate service to show multi service support

---
+++ src/com/java/web/PushService2.java
@@ -0,0 +1,61 @@
+package com.java.web;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import javax.websocket.Session;
+
+public class PushService2 implements Runnable {
+
+	private static PushService2 instance;
+	private static Map<String, Session> sMap = new HashMap<String, Session>();
+
+	private PushService2() {
+	}
+
+	public static void add(Session s) {
+		sMap.put(s.getId(), s);
+	}
+
+	public static void initialize() {
+		if (instance == null) {
+			instance = new PushService2();
+			new Thread(instance).start();
+		}
+	}
+
+	@Override
+	public void run() {
+		Random r = new Random();
+		while (true) {
+			int randomNum = r.nextInt(11);
+			try {
+				for (String key : sMap.keySet()) {
+					Session s = sMap.get(key);
+					if (s.isOpen()) {
+						String pattern = "yyyy-MM-dd hh:m:s";
+						SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+						String d = simpleDateFormat.format(new Date());
+
+						// Date d = new Date(System.currentTimeMillis());
+						s.getBasicRemote().sendText(
+								"[{\"date\":\"" + d.toString() + "\",\"value\":" + Integer.toString(randomNum) + "}]");
+					} else {
+						sMap.remove(key);
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			try {
+				Thread.sleep(1 * 1000);
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
+		}
+	}
+}
